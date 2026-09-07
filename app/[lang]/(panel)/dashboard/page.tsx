fc84b6ae87758928a3c256b3e2c9d58c41a2ee73
hamidwise1 <hamidwise1@gmail.com>
2026-09-07 12:59:24 +0330

    update layout

--- app/[lang]/(panel)/dashboard/page.tsx
+++ app/[lang]/(panel)/dashboard/page.tsx
@@ -1,17 +1,3 @@
-import SidebarProvider from "../../../[lang]/(panel)/services/side-bar/SidebarProvider";
-import AppSidebar from "../../../[lang]/(panel)/services/side-bar/components/AppSidebar";
-import { SidebarInset } from "../../../[lang]/(panel)/services/side-bar/components/Sidebar";
-import Header from "../components/header/Header";
-import MainWrapper from "../components/main/MainWrapper";
-
 export default function Page() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Header />
-        <MainWrapper>test</MainWrapper>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <></>;
 }
---
+++ app/[lang]/(panel)/layout.tsx
@@ -0,0 +1,17 @@
+import SidebarProvider from "../../[lang]/(panel)/services/side-bar/SidebarProvider";
+import AppSidebar from "../../[lang]/(panel)/services/side-bar/components/AppSidebar";
+import { SidebarInset } from "../../[lang]/(panel)/services/side-bar/components/Sidebar";
+import Header from "./components/header/Header";
+import MainWrapper from "./components/main/MainWrapper";
+
+export default function PanelLayout({ children }: LayoutProps<"/[lang]">) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        <MainWrapper>{children}</MainWrapper>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
