455147b2565a7c5b5e5a6d9328ea1d464cfc9f48
hamidwise1 <hamidwise1@gmail.com>
2026-09-08 10:41:43 +0330

    add signin options and signup rediret

--- app/[lang]/(auth)/layout.tsx
+++ app/[lang]/(auth)/layout.tsx
@@ -29,10 +29,10 @@ export default function AuthLayout({ children }: LayoutProps<"/[lang]">) {
               </div>
             </CardContent>
           </Card>
-          <FieldDescription className="px-6 text-center">
-            By clicking continue, you agree to our{" "}
-            <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-          </FieldDescription>
+          {/* <FieldDescription className="px-6 text-center"> */}
+          {/*   By clicking continue, you agree to our{" "} */}
+          {/*   <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>. */}
+          {/* </FieldDescription> */}
         </div>
       </div>
     </div>
