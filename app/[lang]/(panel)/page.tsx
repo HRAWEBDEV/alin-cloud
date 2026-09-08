5de51712f42816e348c664f11a175edb96798a9c
hamidwise1 <hamidwise1@gmail.com>
2026-09-08 09:19:13 +0330

    add meta data dictinary and update manifest file

--- app/[lang]/(panel)/page.tsx
+++ app/[lang]/(panel)/page.tsx
@@ -1,14 +1,3 @@
-import { InputGroupInput, InputGroup } from "@/components/ui/input-group";
-import { Field, FieldLabel } from "@/components/ui/field";
 export default function Home() {
-  return (
-    <div>
-      <Field>
-        <FieldLabel>نام کاربری</FieldLabel>
-        <InputGroup>
-          <InputGroupInput />
-        </InputGroup>
-      </Field>
-    </div>
-  );
+  return <div></div>;
 }
